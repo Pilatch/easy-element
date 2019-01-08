6323e5746b1b87b11f1848295d617933b9a9968c
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-01-08 17:25:57 -0500

    Add in initial support for a slot. Still need to handle a class with no connectedCallback.

---
+++ test/dist/name-tag.class.js
@@ -0,0 +1,30 @@
+class NameTag extends HTMLElement {
+  connectedCallback() {
+    var contents = `
+  <h2 class="name-tag_heading">Hello, my name is</h2>
+  <div class="name-tag_name-container">
+    <slot></slot>
+  </div>
+`;
+
+    if (this.childNodes.length) {
+      var template = document.createElement('div');
+      template.innerHTML = contents;
+      var slot = template.querySelector('slot');
+
+      while (this.childNodes.length) {
+        slot.appendChild(this.firstChild);
+      }
+
+      this.innerHTML = template.innerHTML;
+    } else {
+      this.innerHTML = contents;
+    }
+  } // TODO delete this!
+
+
+}
+
+window.addEventListener('WebComponentsReady', function () {
+  customElements.define('name-tag', NameTag);
+});
---
+++ test/dist/name-tag.es5.js
@@ -0,0 +1,53 @@
+function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
+
+function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
+
+function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
+
+function _possibleConstructorReturn(self, call) { if (call && (typeof call === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+
+function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
+
+function _inherits(subClass, superClass) { if (typeof superClass !== "function" && superClass !== null) { throw new TypeError("Super expression must either be null or a function"); } subClass.prototype = Object.create(superClass && superClass.prototype, { constructor: { value: subClass, writable: true, configurable: true } }); if (superClass) _setPrototypeOf(subClass, superClass); }
+
+function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
+
+let NameTag =
+/*#__PURE__*/
+function (_HTMLElement) {
+  _inherits(NameTag, _HTMLElement);
+
+  function NameTag() {
+    _classCallCheck(this, NameTag);
+
+    return _possibleConstructorReturn(this, (NameTag.__proto__ || Object.getPrototypeOf(NameTag)).apply(this, arguments));
+  }
+
+  _createClass(NameTag, [{
+    key: "connectedCallback",
+    value: function connectedCallback() {
+      var contents = "\n  <h2 class=\"name-tag_heading\">Hello, my name is</h2>\n  <div class=\"name-tag_name-container\">\n    <slot></slot>\n  </div>\n";
+
+      if (this.childNodes.length) {
+        var template = document.createElement('div');
+        template.innerHTML = contents;
+        var slot = template.querySelector('slot');
+
+        while (this.childNodes.length) {
+          slot.appendChild(this.firstChild);
+        }
+
+        this.innerHTML = template.innerHTML;
+      } else {
+        this.innerHTML = contents;
+      }
+    } // TODO delete this!
+
+  }]);
+
+  return NameTag;
+}(HTMLElement);
+
+window.addEventListener('WebComponentsReady', function () {
+  customElements.define('name-tag', NameTag);
+});
