16236ceb4a98e443b7e94ee14e0e36b20b3c13ef
Ethan Martin <Martin.Ethan@pennmutual.com>
2019-01-21 13:57:29 -0500

    Compile down to ES5 better. Duh!

--- test/dist/inner-logger.es5.js
+++ test/dist/inner-logger.es5.js
@@ -1,10 +1,14 @@
+"use strict";
+
+function _typeof(obj) { if (typeof Symbol === "function" && typeof Symbol.iterator === "symbol") { _typeof = function _typeof(obj) { return typeof obj; }; } else { _typeof = function _typeof(obj) { return obj && typeof Symbol === "function" && obj.constructor === Symbol && obj !== Symbol.prototype ? "symbol" : typeof obj; }; } return _typeof(obj); }
+
 function _classCallCheck(instance, Constructor) { if (!(instance instanceof Constructor)) { throw new TypeError("Cannot call a class as a function"); } }
 
 function _defineProperties(target, props) { for (var i = 0; i < props.length; i++) { var descriptor = props[i]; descriptor.enumerable = descriptor.enumerable || false; descriptor.configurable = true; if ("value" in descriptor) descriptor.writable = true; Object.defineProperty(target, descriptor.key, descriptor); } }
 
 function _createClass(Constructor, protoProps, staticProps) { if (protoProps) _defineProperties(Constructor.prototype, protoProps); if (staticProps) _defineProperties(Constructor, staticProps); return Constructor; }
 
-function _possibleConstructorReturn(self, call) { if (call && (typeof call === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
+function _possibleConstructorReturn(self, call) { if (call && (_typeof(call) === "object" || typeof call === "function")) { return call; } return _assertThisInitialized(self); }
 
 function _assertThisInitialized(self) { if (self === void 0) { throw new ReferenceError("this hasn't been initialised - super() hasn't been called"); } return self; }
 
@@ -12,7 +16,7 @@ function _inherits(subClass, superClass) { if (typeof superClass !== "function" 
 
 function _setPrototypeOf(o, p) { _setPrototypeOf = Object.setPrototypeOf || function _setPrototypeOf(o, p) { o.__proto__ = p; return o; }; return _setPrototypeOf(o, p); }
 
-let InnerLogger =
+var InnerLogger =
 /*#__PURE__*/
 function (_HTMLElement) {
   _inherits(InnerLogger, _HTMLElement);
@@ -42,6 +46,8 @@ function (_HTMLElement) {
   return InnerLogger;
 }(HTMLElement);
 
-window.addEventListener('WebComponentsReady', () => {
+"use strict";
+
+window.addEventListener('WebComponentsReady', function () {
   customElements.define('inner-logger', InnerLogger);
 });
