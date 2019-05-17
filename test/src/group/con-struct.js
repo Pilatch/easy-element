e33777d18e21f43922aaa7b39528c99bee5614e6
Pilatch <ethan@pilatch.com>
2019-05-17 11:53:21 -0400

    Add in super() if there's a constructor and it's not already there

---
+++ test/src/group/con-struct.js
@@ -0,0 +1,7 @@
+class ConStruct {
+  constructor() {
+    console.log('I wuz con-structed.')
+    // Look in the console on the demo page.
+    // This shouldn't throw any errors complaining about a lack of super()
+  }
+}
